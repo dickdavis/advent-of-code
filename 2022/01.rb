f9ea203421f3b046674dbbdda7a3985ab1dda947
Dick Davis <dick@hey.com>
2022-12-01 13:13:30 -0600

    Refactor day 1 solution

--- 2022/01.rb
+++ 2022/01.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
-calories_per_elf = [].tap do |arr|
-  elf = 0
-  lines = []
-  File.foreach('01.txt') do |line|
-    unless line.chomp.empty?
-      lines << line.chomp.to_i
-      next
-    end
-
-    arr[elf] = lines.sum
-    elf += 1
-    lines = []
+parsed_input = [].tap do |arr|
+  File.read('01.txt').split("\n\n").each do |group|
+    arr << group.split("\n").map(&:to_i).sum
   end
 end
 
-top_three = calories_per_elf.sort.reverse.take(3)
+top_three = parsed_input.sort { |a, b| b <=> a }.take(3)
 
 puts "Part 1: #{top_three.max} (Expected: 70296)"
 puts "Part 2: #{top_three.sum} (Expected: 205381)"
